8dfb7323ed8f505a9a49a22c424e2ae83cb8f763
Sergey <88627980+T1ktaalik@users.noreply.github.com>
2026-03-16 19:10:04 +0300

    test-3

--- barsa-2-diagram/vite.config.ts
+++ barsa-2-diagram/vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/postcss';
 // https://vite.dev/config/
 export default defineConfig({
+  
   plugins: [
     vue(),
     vueJsx(),
